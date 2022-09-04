dbf8bac3eb9ae9a63ee88ec5f56dabece3b9cce4
hoshogi <hoshogi@gmail.com>
2022-09-04 23:11:13 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/hongik/joinhere/JoinHereApplication.java
@@ -0,0 +1,13 @@
+package com.hongik.joinhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinHereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hongik/joinhere/JoinHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.joinhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
