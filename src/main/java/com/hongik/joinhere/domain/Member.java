132585c1cd2ce91612167901b2da13a1e8bf0980
hoshogi <hoshogi@gmail.com>
2022-09-05 12:33:48 +0900

    feat: Member 도메인 생성

---
+++ src/main/java/com/hongik/joinhere/domain/Member.java
@@ -0,0 +1,52 @@
+package com.hongik.joinhere.domain;
+
+import java.sql.Date;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private String password;
+    private Date birthday;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
