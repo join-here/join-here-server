9ed3f66285232ac282f51be256701dd8cb8bdffb
hoshogi <hoshogi@gmail.com>
2022-09-07 00:59:49 +0900

    feat: club 테이블 sql 추가

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,6 @@
+create database join_here;
+use join_here;
+
 -- member Table Create SQL
 CREATE TABLE member
 (
@@ -6,5 +9,19 @@ CREATE TABLE member
     `password`  VARCHAR(20)    NOT NULL,
     `birthday`  DATE           NOT NULL,
     `phone`     VARCHAR(20)    NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- club Table Create SQL
+CREATE TABLE club
+(
+    `id`            BIGINT           NOT NULL    AUTO_INCREMENT,
+    `name`          VARCHAR(50)      NOT NULL,
+    `category`      CHAR(3)          NOT NULL,
+    `area`          CHAR(2)          NOT NULL,
+    `logo`          BLOB             NULL,
+    `introduction`  VARCHAR(1000)    NULL,
+    `view`          BIGINT           NOT NULL,
+    `scrap`         BIGINT           NOT NULL,
     PRIMARY KEY (id)
 );
