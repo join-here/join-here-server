148b044b4d5ece26ac5d86e0b3666b1961c6df5e
hoshogi <hoshogi@gmail.com>
2022-11-11 21:35:49 +0900

    feat: announcement 테이블 수정

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -35,6 +35,7 @@ CREATE TABLE announcement
     `poster`       MEDIUMBLOB           NULL,
     `start_date`   DATE             NOT NULL,
     `end_date`     DATE             NOT NULL,
+    `inform_state` VARCHAR(1)       NOT NULL,
     `club_id`      BIGINT           NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (club_id) references club(id)
@@ -119,7 +120,7 @@ CREATE TABLE qna_answer
 (
     `id`               BIGINT           NOT NULL    AUTO_INCREMENT,
     `content`          VARCHAR(1000)    NOT NULL,
-    `is_manager`        VARCHAR(1)       NOT NULL,
+    `is_manager`       VARCHAR(1)       NOT NULL,
     `time`             DATETIME         NOT NULL,
     `member_id`        VARCHAR(20)      NOT NULL,
     `qna_question_id`  BIGINT           NOT NULL,
