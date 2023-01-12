ebc097141bb273ea9a0466987fa140e6d37c23e3
hoshogi <hoshogi@gmail.com>
2023-01-12 18:46:50 +0900

    feat: Amazon S3 기본 설정 추가

--- src/main/java/com/hongik/joinhere/JoinHereApplication.java
+++ src/main/java/com/hongik/joinhere/JoinHereApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JoinHereApplication {
 
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(JoinHereApplication.class, args);
 	}
-
 }
