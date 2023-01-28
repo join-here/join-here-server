177d01b4ce98e3d562d4a5e208946c0942a1c934
hoshogi <hoshogi@gmail.com>
2023-01-28 15:06:52 +0900

    refactor: 동아리 등록 dto 수정

--- src/main/java/com/hongik/joinhere/dto/club/CreateClubResponse.java
+++ src/main/java/com/hongik/joinhere/dto/club/CreateClubResponse.java
@@ -9,15 +9,8 @@ import lombok.Getter;
 public class CreateClubResponse {
 
     private Long id;
-    private String name;
-    private String category;
-    private String area;
-    private String imageUrl;
-    private String introduction;
-    private Long view;
-    private Long scrap;
 
     public static CreateClubResponse from(Club club) {
-        return new CreateClubResponse(club.getId(), club.getName(), club.getCategory(), club.getArea(), club.getImageUrl(), club.getIntroduction(), club.getView(), club.getScrap());
+        return new CreateClubResponse(club.getId());
     }
 }
